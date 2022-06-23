37514175631a7b1d03256c55dcef2bfd7be103f8
kevinpryan <kevinpa.ryan@gmail.com>
2022-06-23 15:57:32 +0100

    making batch folder, putting trim galore batch script in

--- strandedness/list_allfiles.R
+++ strandedness/list_allfiles.R
@@ -1,6 +1,8 @@
 #!/usr/bin/Rscript
-
-read1 <- list.files(path = "/data/kryan/rna_seq_bc/caf_subtypes/EGAD00001005744", full.names=T, recursive = T, pattern = "R1.fastq.gz$")
-read2 <- list.files(path = "/data/kryan/rna_seq_bc/caf_subtypes/EGAD00001005744", full.names=T, recursive = T, pattern = "R2.fastq.gz$")
-all_files <- cbind.data.frame(read1, read2)
-write.table(all_files, file = "/data/kryan/rna_seq_bc/caf_subtypes/EGAD00001005744/all_files_combined_EGAD00001005744.txt", quote = F, sep = "\t", row.names = F)
+library(stringr)
+read1 <- list.files(path = "/data/kryan/rna_seq_bc/caf_subtypes/EGAD00001004810", full.names=T, recursive = T, pattern = "R1.fastq.gz$")
+read2 <- list.files(path = "/data/kryan/rna_seq_bc/caf_subtypes/EGAD00001004810", full.names=T, recursive = T, pattern = "R2.fastq.gz$")
+sample_files <- str_split_fixed(string = read1, pattern = "/", n = 8)[,8]
+sample_name <- str_split_fixed(sample_files, pattern = "\\.", n = 4)[,1]
+all_files <- cbind.data.frame(sample_name, read1, read2)
+write.table(all_files, file = "/data/kryan/rna_seq_bc/caf_subtypes/EGAD00001004810/all_files_combined_EGAD00001004810.txt", quote = F, sep = "\t", row.names = F)
