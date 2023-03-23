4fe2b80af090b743f0b739973ea21862de5493ca
Kevin Ryan <kevinpa.ryan@gmail.com>
2023-03-23 18:15:20 +0000

    fix gigabase script

--- useful/convert_base_to_gigabase.py
+++ useful/convert_base_to_gigabase.py
@@ -3,19 +3,34 @@ import csv
 filename = "count_bases.txt"
 
 # read in file as dictionary - every second line is filename, bases
-with open(filename) as f:
-    counts = {int(name.strip()):score.strip() for score, name in zip(f, f)}
+#with open(filename) as f:
+    #counts = {int(name.strip()):score.strip() for score, name in zip(f, f)}
+ #   counts = {int(name.strip()):score.strip() for score, name in zip(f,f)}
+#print(counts)
 
+my_dict = {}
+with open(filename) as f:
+    lines = f.readlines()
+    for i in range(0, len(lines), 2):
+        try:
+            sample_name = lines[i].strip()
+            number_of_bases = int(lines[i+1])
+            my_dict[sample_name] = number_of_bases
+        except IndexError:
+            print(f"Skipping last line: {lines[i].strip()}")
+
+
+#print(my_dict)
 # I want Sample to be key and bases to be value
-inv_map = {v: k for k, v in counts.items()}
+#inv_map = {v: k for k, v in counts.items()}
 
 gigabases_dict = {}
 gigabases_list = []
 
 # create list of dictionaries containing gigabases for each sample/file
-for x in inv_map:
+for x in my_dict:
     gigabases_dict["Sample"] = x
-    gigabases_dict["Gigabases"] = str(round(inv_map[x]/1e9, 2))
+    gigabases_dict["Gigabases"] = str(round(my_dict[x]/1e9, 2))
     gigabases_list.append(gigabases_dict)
     gigabases_dict = {}
 
