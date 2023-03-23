84eefb496b6178e881b79712db4cefdee02f98ef
Kevin Ryan <kevinpa.ryan@gmail.com>
2023-03-23 17:44:47 +0000

    add reference biostars post to count bases

--- useful/fastq_count_bases.sh
+++ useful/fastq_count_bases.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+# reference biostars post: https://www.biostars.org/p/78043/#78051 
 INPUT_DIR=/mnt/data/wes_data_copy/
 for x in ${INPUT_DIR}*.fastq; do echo "$(basename -- $x)" && cat "$x" | paste - - - - | cut -f 2 | tr -d '\n' | wc -c; done > count_bases.txt
