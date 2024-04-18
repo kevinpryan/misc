63ade0d21c5cb662bddb7479535939231e19d58a
Kevin Ryan <kevinpa.ryan@gmail.com>
2024-04-18 15:23:16 +0100

    add erv info

---
+++ useful/healed/erv/README.md
@@ -0,0 +1,17 @@
+# Endogenous retroviruses
+List of annotated endogenous retroviral genes downloaded from https://g-a8b222.dd271.03c0.data.globus.org/pub/databases/genenames/hgnc/tsv/locus_types/endogenous_retrovirus.txt one 18/04/2024
+
+run:
+
+```{bash}
+check_ervs.sh
+```
+
+Output:
+
+```{console}
+Number of ERVs in the panel:
+10
+Number of ERVs not in the panel:
+287869
+```
---
+++ useful/healed/erv/check_ervs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+# get BED file for the enrichment panel
+wget https://emea.support.illumina.com/content/dam/illumina-support/documents/downloads/productfiles/illumina-prep/exome/hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated.BED 
+# get list of ervs
+wget https://g-a8b222.dd271.03c0.data.globus.org/pub/databases/genenames/hgnc/tsv/locus_types/endogenous_retrovirus.txt
+# extract second column of endogenous_retrovirus.txt excluding the header
+cut -f 2 endogenous_retrovirus.txt | tail -n +2 > endogenous_retrovirus_in_hgnc.txt
+# count the number of ERVs in the panel
+echo "Number of ERVs in the panel:"
+grep -f endogenous_retrovirus_in_hgnc.txt hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated.BED | wc -l
+# write the ERVs in the panel to a file
+grep -f endogenous_retrovirus_in_hgnc.txt hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated.BED > ERVs_in_panel.bed
+# count the number of ERVs not in the panel
+echo "Number of ERVs not in the panel:"
+grep -v -f endogenous_retrovirus_in_hgnc.txt hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated.BED | wc -l
+# clean up files
+rm hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated.BED endogenous_retrovirus_in_hgnc.txt endogenous_retrovirus.txt
